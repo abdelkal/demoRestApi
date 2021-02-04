b9b40dec375725bf0ca8c00e13ba2b8545f62524
abdelkal <abdelilahkal@gmail.com>
2021-02-04 23:13:26 +0100

    Hello World Rest Resource

--- src/main/java/be/unit/demo/controller/DemoController.java
+++ src/main/java/be/unit/demo/controller/DemoController.java
@@ -1,7 +1,13 @@
 package be.unit.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
